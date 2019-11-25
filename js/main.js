962ff6cecf1bf67e6ac6966c6853646f284fe761
tania <tani.zhelezniak@gmail.com>
2019-11-25 10:40:22 +0200

    prototype

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('test');
+const person = new Object({
+    name: 'alex',
+    age: 'age',
+    greet: function () {
+        console.log('greet');
+    }
+});
+
+Object.prototype.sayHello = function () {
+    console.log('hello');
+}
+
+
+const lena = Object.create(person);
