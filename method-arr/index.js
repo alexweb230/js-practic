8a13c5fc54be281346e7d47d36822bf71b8ef511
tania <tani.zhelezniak@gmail.com>
2019-12-03 10:34:08 +0200

    method arr

---
+++ method-arr/index.js
@@ -0,0 +1,26 @@
+const people = [
+    {name: "alex", age: 25, job: 'front'},
+    {name: "alex-1", age: 24, job: 'front-1'},
+    {name: "alex-2", age: 26, job: 'front-2'},
+    {name: "alex-3", age: 27, job: 'front-3'},
+    {name: "alex-4", age: 27, job: 'front-4'},
+    {name: "alex-5", age: 29, job: 'front-5'},
+]
+
+// for (let i = 0; i < people.length; i++) {
+//
+//     console.log(people[i]);
+//     console.log('for')
+// }
+
+
+// for (let person of people){
+//     console.log(person);
+//     console.log('for in')
+// }
+
+
+// people.forEach(person => {
+//     console.log(person);
+//     console.log('forEach')
+// })
