38278fab8a689c3ee0deab06b072d6623300b35f
tania <tani.zhelezniak@gmail.com>
2019-12-04 10:57:24 +0200

    my form

---
+++ index.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+
+
+
+
+
+}
+class FormBuilder {
+    constructor(options) {
+        this.formId = options.formId
+
+    }
+
+    get formParent() {
+        return document.getElementById(this.formId);
+    }
+
+    validator(){
+        return console.log(this.formParent)
+    }
+
+}
+
+
+const form = new FormBuilder({
+    formId: 'form',
+    formParent: 'form'
+});
+
