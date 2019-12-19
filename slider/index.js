70d286f5aa8d4ad395d94c68688502d3c9eb92b4
tania <tani.zhelezniak@gmail.com>
2019-12-19 10:42:42 +0200

    method arr

--- slider/index.js
+++ slider/index.js
@@ -1,30 +1,47 @@
 'use strict';
 
 class sliderCarusel {
-    constructor({main, wrap}) {
-      this.main = document.querySelector(main);
-      this.wrap = document.querySelector(wrap);
-      this.slides = document.querySelector(wrap).children;
+    constructor({main, wrap, position = 0}) {
+        this.main = document.querySelector(main);
+        this.wrap = document.querySelector(wrap);
+        this.slides = document.querySelector(wrap).children;
+        this.position = position;
 
     }
+    init() {
+        this.addGloClass();
+        this.addStyle();
+    }
 
-    addGloClass(){
-        this.main.classList.add('.glo-slider');
-        this.wrap.classList.add('.glo-slider--wrap');
-        for(let item of this.slides){
-            item.classList.add('.glo-slider--item');
+    addGloClass() {
+        this.main.classList.add('glo-slider');
+        this.wrap.classList.add('glo-slider--wrap');
+        for (let item of this.slides) {
+            item.classList.add('glo-slider--item');
         }
     }
-    addStyle(){
+
+    addStyle() {
         const style = document.createElement('style');
         style.id = 'sliderCarousel';
+        style.textContent = `
+           .glo-slider{
+              overflow: hidden !important;
+           }
+           .glo-slider--wrap{
+              display: flex !important;
+              transition: transform .5s !important;    
+              will-change:transform !important;
+           }
+           .glo-slider--item{
+              margin: auto 0 !important;
+              flex: 0 0 25% !important;
+           }
+        `;
         document.head.appendChild(style);
     }
 
-    init() {
-        this.addStyle();
-        this.addGloClass();
-    }
+
 }
 
 
