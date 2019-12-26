926607ade96b1b67ed5f49165564b326fdba1611
tania <tani.zhelezniak@gmail.com>
2019-12-26 16:06:29 +0200

    tabs

--- tabs/index.js
+++ tabs/index.js
@@ -1,10 +1,13 @@
 class Tabs {
     constructor(options){
-        this.event = options.event
-        this.parent = options.parent
+        this.event = options.event;
+        this.parent = options.parent;
     }
     get navItem(){
-        return document.querySelectorAll(`#${this.parent} .tabs-nav li`);
+        return document.querySelectorAll(`${this.parent} .tabs-nav li`);
+    }
+    get  tabsBox(){
+        return document.querySelectorAll(`${this.parent} .tabs-box .tabs-box--item`);
     }
     events(){
 
@@ -13,6 +16,14 @@ class Tabs {
                 e.preventDefault();
                 array.forEach(ar => ar.classList.remove('is--active'));
                 item.classList.add('is--active');
+
+                this.tabsBox.forEach((t, ind) => {
+                    t.classList.remove('is--active');
+                    if(index === ind){
+                        t.classList.add('is--active');
+                    }
+                });
+
             });
 
         });
@@ -25,6 +36,30 @@ class Tabs {
 
 const tabs = new Tabs({
     event: 'click',
-    parent: 'tabs'
+    parent: '#tabs'
 });
 tabs.init();
+
+
+const tabs2 = new Tabs({
+    event: 'mouseover',
+    parent: '#tabs2'
+});
+
+tabs2.init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
