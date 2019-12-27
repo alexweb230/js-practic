051cd0f7369af27e23d82fdbf166f12160b2235a
tania <tani.zhelezniak@gmail.com>
2019-12-27 15:47:42 +0200

    prototype

--- player.js
+++ player.js
@@ -3,43 +3,46 @@ class Video {
         this.$parentId = document.querySelector(options.parentId);
     }
 
-    get buttonPlay(){
-        return document.querySelector(`${this.$parentId}`);
-    }
-
-
+    video;
+    controlElement = {
+        btnPlay: '<button class="but-play">play</button>',
+        small: '<small> </small>'
+    };
 
     addVideo() {
         let player = document.createElement('video');
         player.src = this.$parentId.dataset.src;
         this.$parentId.prepend(player);
+        this.video = player;
     }
 
     addControls() {
         let controls = document.createElement('div');
         controls.className = 'video-player-controls';
-        controls.innerHTML = '<button class="but-play">play</button>';
+
+        for(let it in this.controlElement){
+            controls.append(this.controlElement[it]);
+            console.log(it)
+        }
+
 
         this.$parentId.append(controls);
 
-        console.log(this.buttonPlay)
+
 
     }
 
+
+
     playVideo(){
-        this.$parentId.addEventListener('click',  e => {
-            let target = e.target;
-            if(target === this.buttonPlay){
-                console.log('test');
-            }
-        })
+      console.log(this.controlElement)
     }
 
 
     init() {
         this.addVideo();
         this.addControls();
-        // this.playVideo();
+         this.playVideo();
     }
 }
 
