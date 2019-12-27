18f763d32bc6a8ceb360871cc53cb2366cea869a
tania <tani.zhelezniak@gmail.com>
2019-12-27 15:53:50 +0200

    prototype

--- player.js
+++ player.js
@@ -19,30 +19,28 @@ class Video {
     addControls() {
         let controls = document.createElement('div');
         controls.className = 'video-player-controls';
-
-        for(let it in this.controlElement){
-            controls.append(this.controlElement[it]);
-            console.log(it)
+        let control;
+        for (let it in this.controlElement) {
+            control = this.controlElement[it];
+            console.log(control);
         }
-
+         controls.innerHTML = control;
 
         this.$parentId.append(controls);
-
 
 
     }
 
 
-
-    playVideo(){
-      console.log(this.controlElement)
+    playVideo() {
+        console.log(this.controlElement)
     }
 
 
     init() {
         this.addVideo();
         this.addControls();
-         this.playVideo();
+        this.playVideo();
     }
 }
 
