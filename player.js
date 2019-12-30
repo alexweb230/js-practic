7b534772f77aff6169c5dfde7ebac8c23672ef7c
tania <tani.zhelezniak@gmail.com>
2019-12-30 16:55:02 +0200

    prototype

--- player.js
+++ player.js
@@ -1,13 +1,13 @@
 class Video {
     constructor(options) {
         this.$parentId = document.querySelector(options.parentId);
+        this.width = options.width;
     }
 
     arrBuild(arg) {
         return Array.from(arg);
     }
 
-    video;
 
     controlTemplate = ` <button class="but-play is-paused">sss</button>
                     <small>sss</small>
@@ -18,9 +18,11 @@ class Video {
 
     addVideo() {
         let player = document.createElement('video');
+        player.className = 'video';
         player.src = this.$parentId.dataset.src;
         this.$parentId.prepend(player);
-        this.video = player;
+        player.style.width = this.width;
+
     }
 
     addControls() {
@@ -32,17 +34,18 @@ class Video {
 
 
     play() {
-         this.$parentId.addEventListener('click', e => {
-             let target = e.target;
-             if(target === this.video){
-                 this.video.play();
-                 console.log(this)
-             }
+        this.$parentId.addEventListener('click', e => {
+            let target = e.target;
+            let btn = document.querySelector(`#${this.$parentId.id} .but-play`);
+            let player = document.querySelector(`#${this.$parentId.id} .video`);
 
-         });
+            if (target === player || target === btn) {
+                player.play();
+                console.log(btn)
+            }
+
+        });
     }
-
-
 
 
     init() {
@@ -53,5 +56,7 @@ class Video {
 }
 
 const video = new Video({
-    parentId: '#video'
+    parentId: '#video',
+    width: '100%'
+
 }).init();
