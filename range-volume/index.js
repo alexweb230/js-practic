b546912d4d65a4be694750de834207a3090ced82
tania <tani.zhelezniak@gmail.com>
2020-01-13 09:22:29 +0200

    range-volume

--- range-volume/index.js
+++ range-volume/index.js
@@ -1,4 +1,11 @@
 let range = document.querySelector('.range-box');
 let moveProgress = document.querySelector('.range');
 
-console.log(range)
+range.addEventListener('mousedown',  rangeMove);
+
+
+
+
+function rangeMove(e) {
+    console.log(e.offsetX / 2);
+}
