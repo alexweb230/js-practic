c3960ed8450ba1e8a3429e2d1024cdead23988a9
tania <tani.zhelezniak@gmail.com>
2020-01-22 16:17:30 +0200

    method arr

--- my-slider/index.js
+++ my-slider/index.js
@@ -1,4 +1,47 @@
+class AlexSlider {
+    constructor(options) {
+        this.wrapper = options.wrapper
+    }
 
+    addStyles() {
+        let style = document.createElement('style');
+        style.id = 'alex-slider';
+        style.textContent = `
+               .alex-slider--wrapper {
+                    overflow: hidden;
+                    max-width: 1000px;
+                    border: 2px solid;
+                    margin: auto;
+                }
+
+                .alex-slider--container {
+                    display: flex;
+                    transition: transform .5s;
+                    will-change: transform;
+                
+                }
+
+                .alex-slider--item {
+                    flex-basis: 25%;
+                    flex-shrink: 0;
+                    margin: auto 0;
+                
+                }
+
+                .alex-slider--item img {
+                    width: 100%;
+                }`;
+        document.head.append(style);
+    }
+
+
+    init() {
+        this.addStyles();
+    };
+
+}
+
+const slider = new AlexSlider({}).init();
 
 
 
