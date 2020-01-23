8567b273d582c1e26130213806f6142f3c3e5e09
tania <tani.zhelezniak@gmail.com>
2020-01-23 11:38:46 +0200

    method arr

--- my-slider/index.js
+++ my-slider/index.js
@@ -1,24 +1,29 @@
 class AlexSlider {
     constructor(options) {
         this.wrapperInit = options.selector;
+        this.arrows = options.arrows;
     }
 
-    // добавление стилей
+    // елементы разметки
     get parent() {
         return document.querySelector(`${this.wrapperInit}`);
     }
-    get prev(){
-        // return document.querySelector(`${this.wrapperInit}`);
+    get prev() {
+        return document.querySelector(`${this.wrapperInit} .alex-btn--prev`);
     }
 
-    get next(){
-
+    get next() {
+        return document.querySelector(`${this.wrapperInit} .alex-btn--next`);
+    }
+    get slideWidth(){
+        let slider = document.querySelector(`${this.wrapperInit} .alex-slider--item`);
+        return slider.offsetWidth;
     }
 
     //добавление стилей слайдера
     addStyles() {
         let style = document.getElementById('alex-slider');
-        if(!style){
+        if (!style) {
             style = document.createElement('style');
             style.id = 'alex-slider';
         }
@@ -50,31 +55,56 @@ class AlexSlider {
 
         document.head.append(style);
     }
+    // добавление кнопок
+    addArrows() {
+        let btnPrev = document.createElement('button');
+        let btnNext = document.createElement('button');
 
-    addArrows(){
-      let arrows = `
-         <button class="alex-btn--prev">prev</button>
-         <button class="alex-btn--next">next</button>
-      `;
-       this.parent.append(arrows);
+        btnPrev.className = "alex-btn--prev";
+        btnNext.className = "alex-btn--next";
+
+        btnPrev.textContent = "prev";
+        btnNext.textContent = "next";
+
+        this.parent.append(btnPrev);
+        this.parent.append(btnNext);
 
     }
 
+    //пролистывание в право
+    prevSlide(){
+        this.prev.addEventListener('click',  () => {
+            console.log(this.slideWidth)
+        });
+    }
+    nextSlide(){
+        this.next.addEventListener('click',  () => {
+            console.log('next')
+        });
+    }
 
+    //инит слайдера
     init() {
         this.addStyles();
-        this.addArrows();
+
+        if(this.arrows){
+            this.addArrows();
+        }
+        this.prevSlide();
+        this.nextSlide();
     };
 
 }
 
 const slider = new AlexSlider({
     selector: '.my-slider',
+    arrows: true,
 }).init();
 
 
 const slider2 = new AlexSlider({
     selector: '.my-slider-2',
+    arrows: true,
 }).init();
 
 
