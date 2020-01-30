7646c83377b826d355694b61e0ddafe15c3e0bc7
tania <tani.zhelezniak@gmail.com>
2020-01-30 16:11:39 +0200

    method arr

--- range/index.js
+++ range/index.js
@@ -8,16 +8,17 @@ document.addEventListener('DOMContentLoaded', () => {
     rangeEnd.addEventListener('change', heandler);
 
 
+    const result = () => {
+        console.log('test');
+    }
 
 
    function heandler() {
         if(rangeStart.value >  rangeEnd.value || rangeEnd.value < rangeStart.value){
             rangeEnd.value = rangeStart.value = this.value;
         }
+        result();
    }
-
-
-
 
 
 
